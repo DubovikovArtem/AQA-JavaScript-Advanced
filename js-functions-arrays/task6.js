d3a16ef9242d36caa2e881ba77249c9d1bbe4573
DubovikovArtem <dubovikov.arm@gmail.com>
2023-10-03 17:03:26 +0300

    HW 5 crate new folder js-functions-arrays add 7 files

---
+++ js-functions-arrays/task6.js
@@ -0,0 +1,6 @@
+const arrayFirst = ["test1", "test2", "test3"];
+const arraySecond = ["test1", "test1", "test1"];
+
+const concatNewArray = arrayFirst.concat(arraySecond);
+
+console.log(concatNewArray);
